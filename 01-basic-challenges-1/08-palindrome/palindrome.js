755b6ed31d66f1fe9d00168c2fa217139551ab53
Andres <andrespodadera87@gmail.com>
2023-12-18 22:20:44 +0100

    challengue palindrome solved

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,8 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    const formatted = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversed = formatted.split('').reverse().join('');
+
+    return formatted === reversed;
+}
 
 module.exports = isPalindrome;
